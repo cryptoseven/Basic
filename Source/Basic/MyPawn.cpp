f6eafd474f212618a15670955c7b79484df146f2
andreas <andreasw87@gmail.com>
2019-02-17 16:16:25 +0100

    Pawn Actor and Blueprint
    Pawn with mesh, camera and movement

---
+++ Source/Basic/MyPawn.cpp
@@ -0,0 +1,48 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "MyPawn.h"
+#include "Components/InputComponent.h"
+#include "GameFramework/FloatingPawnMovement.h"
+
+// Sets default values
+AMyPawn::AMyPawn()
+{
+ 	// Set this pawn to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	CreateDefaultSubobject<UFloatingPawnMovement>("Movement");
+	mesh = CreateDefaultSubobject<UStaticMeshComponent>("Mesh");
+	cam = CreateDefaultSubobject<UCameraComponent>("Cam");
+}
+
+// Called when the game starts or when spawned
+void AMyPawn::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AMyPawn::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+// Called to bind functionality to input
+void AMyPawn::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+	PlayerInputComponent->BindAxis("MoveForward", this, &AMyPawn::moveForward);
+	PlayerInputComponent->BindAxis("MoveRight", this, &AMyPawn::moveRight);
+}
+
+void AMyPawn::moveForward(float value) {
+	AddMovementInput(GetActorForwardVector(), value);
+}
+
+void AMyPawn::moveRight(float value) {
+	AddMovementInput(GetActorRightVector(), value);
+}
+
---
+++ Source/Basic/MyPawn.h
@@ -0,0 +1,38 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Pawn.h"
+#include "Camera/CameraComponent.h"
+#include "MyPawn.generated.h"
+
+class UCameraComponent;
+
+UCLASS()
+class BASIC_API AMyPawn : public APawn
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this pawn's properties
+	AMyPawn();
+	UPROPERTY(EditAnywhere)
+	UStaticMeshComponent* mesh;
+	UPROPERTY(EditAnywhere)
+	UCameraComponent* cam;
+
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	// Called to bind functionality to input
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+	void moveForward(float value);
+	void moveRight(float value);
+};
